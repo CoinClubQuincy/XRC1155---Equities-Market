458d2ce0e2fe1745726a09169a305927b6add707
Quincy Jones <rqjones@asu.edu>
2023-08-09 19:09:13 -0700

    Update portfolio.sol

--- contracts/portfolio.sol
+++ contracts/portfolio.sol
@@ -1,7 +1,5 @@
 //SPDX-License-Identifier: UNLICENSED
 
-// Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
